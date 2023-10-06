1379024dbd30e308ba577351ea879fc502e21556
mjsydney23 <michael.h.jin@gmail.com>
2023-10-06 20:00:38 +1100

    One-liner Web Server

--- chp_02/main.tf
+++ chp_02/main.tf
@@ -3,10 +3,31 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0e812285fd54f7620"
-  instance_type = "t2.micro"
+  ami                    = "ami-0d02292614a3b0df1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
