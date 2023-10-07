6665940ce2bf6849eb1de8eb181bf5b86b83bbe7
mjsydney23 <michael.h.jin@gmail.com>
2023-10-07 21:05:30 +1100

    Cluster Web Servers

--- chp_02/main.tf
+++ chp_02/main.tf
@@ -2,22 +2,45 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0d02292614a3b0df1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0d02292614a3b0df1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+    EOF
 
-  user_data_replace_on_change = true
+  # Required when using a launch configuration with an auto scalinng group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
@@ -35,9 +58,4 @@ resource "aws_security_group" "instance" {
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
-}
-
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
 }
