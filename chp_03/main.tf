48cf1eb4d2875379644ec1d2bf164cd23d906d7d
mjsydney23 <michael.h.jin@gmail.com>
2023-10-09 08:23:18 +1100

    Remote Backend Config

--- chp_03/main.tf
+++ chp_03/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "mjsydney23-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "ap-southeast-2"
+
+    dynamodb_table = "mjsydney23-terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
@@ -45,4 +56,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
